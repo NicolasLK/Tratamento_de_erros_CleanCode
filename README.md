4d4b235be158a87d34132e80f925c25d6a6bc28e
NicolasLK <nicolas.kaminski.2001@gmail.com>
2025-04-12 15:24:04 -0300

    feat: Adaptando contexto para tipos diferentes de logs.

--- README.md
+++ README.md
@@ -40,6 +40,7 @@ Este projeto é um pequeno programa escrito em **TypeScript** que simula um torn
 Tratamento_de_erros/
 ├── logs/
 │   └── error.log
+│   └── info.log
 ├── src/
 │   ├── models/
 │   │   ├── match.ts
--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,9 @@
+import * as path from "path";
 import { createLogger, format, transports } from "winston";
 
 export function createContextLogger(context: string) {
-  return createLogger({
-    level: "info",
+  const logger = createLogger({
+    level: "silly",
     format: format.combine(
       format.label({ label: context }),
       format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
@@ -10,6 +11,25 @@ export function createContextLogger(context: string) {
         return `[${timestamp}] [${label}] ${level.toUpperCase()}: ${message}`;
       })
     ),
-    transports: [new transports.Console()],
+    transports: [
+      new transports.Console(),
+      new transports.File({
+        filename: path.resolve(__dirname, "../logs/error.log"),
+        level: "error",
+      }),
+      new transports.File({
+        filename: path.resolve(__dirname, "../logs/info.log"),
+        level: "info",
+        format: format((info) => {
+          return info.level === "info" ||
+            info.level === "debug" ||
+            info.level === "silly"
+            ? info
+            : false;
+        })(),
+      }),
+    ],
   });
+
+  return logger;
 }
